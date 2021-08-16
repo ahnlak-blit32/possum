21f6bca927a02d81a0faa5b5af3f4ec255bf21e7
Pete Favelle <pete@fsquared.co.uk>
2021-08-16 17:07:29 +0100

    Added redimentary pause button

--- possum.cpp
+++ possum.cpp
@@ -19,6 +19,8 @@
 #define  _POSSUM_X
 #include "possum.hpp"
 
+#include "graphics/font.hpp"
+
 #include "background.hpp"
 #include "player.hpp"
 #include "ssap.hpp"
@@ -28,6 +30,7 @@
 
 Background *m_background = nullptr;
 Player     *m_player = nullptr;
+bool        m_running = true;
 
 
 /* Functions. */
@@ -80,6 +83,19 @@ void update( uint32_t p_time )
     m_background->update( p_time );
   }
 
+  /* The pause button is a universal control (button Y, nearest the screen) */
+  if ( blit::buttons.pressed & blit::Button::Y )
+  {
+    /* Toggle our run state. */
+    m_running = !m_running;
+  }
+
+  /* And if we're paused, no more to do. */
+  if ( !m_running )
+  {
+    return;
+  }
+
   /* When the game isn't running, the player object still needs updates. */
   if ( m_player != nullptr )
   {
@@ -106,6 +122,21 @@ void render( uint32_t p_time )
   if ( m_background != nullptr )
   {
     m_background->render();
+  }
+
+  /* And if we're paused, just need an 'un-pause' prompt. */
+  if ( !m_running )
+  {
+    /* Set to a flickering pen. */
+    blit::screen.pen.r = blit::screen.pen.b = blit::screen.pen.a = 255;
+    blit::screen.pen.g = p_time % 256;
+
+    /* And clue the player in. */
+    blit::Point l_text_pos( blit::screen.bounds.w / 2, ( blit::screen.bounds.h / 2 ) - 8 );
+    blit::screen.text( "PAUSED", blit::minimal_font, l_text_pos, true, blit::TextAlign::center_center );
+    l_text_pos.y += 16;
+    blit::screen.text( "<Y> TO RESUME", blit::minimal_font, l_text_pos, true, blit::TextAlign::center_center );
+    return;
   }
 
   /* If the game isn't running, the player still needs to be visible. */
